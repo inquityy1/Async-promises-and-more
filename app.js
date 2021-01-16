ee6ed7023a562746b9a03d18c22377fb634a4cd4
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-17 00:05:57 +0100

    Async await

--- app.js
+++ app.js
@@ -24,24 +24,27 @@ const setTimer = (duration) => {
 	return promise;
 };
 
-function trackUserHandler() {
-  let positionData;
-  getPosition()
-	.then(posData => {
-	  positionData = posData;
-	  return setTimer(2000);
-	})
-	.catch(err => {
-		console.log(err);
-		return 'on we go...';
-	})
-	.then(data => {
-	  console.log(data, positionData);
-	});
-  setTimer(1000).then(() => {
-	console.log('Timer done!');
-  });
-  console.log('getting position...');
+async function trackUserHandler() {
+  //let positionData;
+  const posData = await getPosition();
+  const timerData = await setTimer(2000);
+  console.log(timerData, posData);
+  
+//	.then(posData => {
+//	  positionData = posData;
+//	  return setTimer(2000);
+//	})
+//	.catch(err => {
+//		console.log(err);
+//		return 'on we go...';
+//	})
+//	.then(data => {
+	//  console.log(data, positionData);
+	//});
+  //setTimer(1000).then(() => {
+//	console.log('Timer done!');
+ // });
+  //console.log('getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
